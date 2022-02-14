4698f6840027caaba0c85eaac42eb415189b522a
Orestis <40428588@live.napier.ac.uk>
2022-02-14 06:22:55 +0000

    Add project files.

---
+++ Domain/Entities/Ingredient.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Ingredient
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Category { get; set; }
+        public List<ItemIngredient> ItemIngredients { get; set; }
+    }
+}
---
+++ Domain/Entities/ItemIngredient.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class ItemIngredient
+    {
+        public int MenuItemId { get; set; }
+        public MenuItem MenuItem { get; set; }
+        public int IngredientId { get; set; }
+        public Ingredient Ingredient { get; set; }
+    }
+}
